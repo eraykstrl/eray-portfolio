26af86c8563f8a6ea326eaf9c83175e84ab7716b
eraykstrl <kosterelieray61@gmail.com>
2026-06-16 17:43:24 +0300

    Index file was updated

--- eray-portfolio/src/pages/Home.jsx
+++ eray-portfolio/src/pages/Home.jsx
@@ -1,14 +1,1 @@
-import Hero from "../sections/Hero";
-import Stats from "../sections/Stats";
-
-function Home() {
-  return(
-    <main className="flex-1 space-y-20">
-      <Hero />
-      <Stats />
-    </main>
-  );
-
-}
-
-export default Home;
+ 
